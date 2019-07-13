52623e1851eb60aa1e015e2a0a980d306f612bab
Kevin <nelson67@stolaf.edu>
2019-07-13 13:38:32 -0500

    sequecial nested hashes

--- test.rb
+++ test.rb
@@ -24,15 +24,16 @@ class Parser
   end
 
   def parse_file(line)
-    return line if @index == @file_length
-
     parsed_line = parse_line(@file[@index])
     key = parsed_line[:key]
     value = parsed_line[:value]
 
+    return key if @index == @file_length - 1
+
     file_hash = {}
-    @index += 1
-    file_hash[key] = { "#{value}": parse_file(@file[@index + 1]) }
+    file_hash[key] = { "#{value}": parse_file(@file[@index += 1]) }
     p file_hash
   end
 end
+
+puts Parser.new.parse_file("test.txt")
